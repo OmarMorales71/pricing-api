87c4c05f6dd7e3e4911ba04925c4776cce530e1d
Omar Morales Ortega <morales.ortega.omar@gmail.com>
2026-09-24 20:45:24 +0200

    Initial Spring Boot project

---
+++ src/test/java/com/omarmorales/pricingapi/PricingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omarmorales.pricingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
